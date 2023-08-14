ecc1180b990bed04d12e304c1c716b01acd8e9b8
Recker4244 <ashutoshsenapati2311@gmail.com>
2023-08-14 23:37:41 +0530

    feat: error page

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -3,13 +3,13 @@ import { useParams } from 'react-router-dom';
 import './ErrorPage.css';
 
 function ErrorPage() {
-  const { errorCode } = useParams();
-  return (
-    <div className="errorContainer">
-      <p className="errorText">Something went wrong!</p>
-      {errorCode && <p className="errorText">{`Error code: ${errorCode}`}</p>}
-    </div>
-  );
+	const { errorCode } = useParams();
+	return (
+		<div className="errorContainer">
+			<p className="errorText">Something went wrong!</p>
+			{errorCode && <p className="errorText">{`Error code: ${errorCode}`}</p>}
+		</div>
+	);
 }
 
 export default ErrorPage;
