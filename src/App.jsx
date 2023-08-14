75fbd5822db29c1212a98e5172c33001be558506
Recker4244 <ashutoshsenapati2311@gmail.com>
2023-08-15 01:07:28 +0530

    feat: query editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route ,Routes} from 'react-router-dom';
+import Pages from './pages/index.js';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World!</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path='*' element={<Pages.HomePage />} />
+					<Route path='/editor' element={<Pages.EditorPage />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
--- src/components/QueryInput/index.jsx
+++ src/components/QueryInput/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import './QueryInput.css';
+import CodeMirror from '@uiw/react-codemirror';
+import MainContext from '../../MainContext';
+import EditorPanel from '../Panel';
 
-function QueryInput() {
-	return <div className='query-input'>
-		<input type="text" placeholder="SQL Query" ></input>
-	</div>;
-}
+const QueryInput=()=> {
+	const { query, setQuery } = useContext(MainContext);
+	return(<div className='editor'>
+		
+		<div className='query-input'>
+			<CodeMirror
+				value={query}
+				name='Editor'
+				onBeforeChange={(editor, data, value) => setQuery(value)}
+				className='code-mirror-wrapper'
+				options={{
+					lint: true,
+					mode: 'sql',
+					lineNumbers: true,
+					keyMap: 'sublime',
+					matchBrackets: true,
+					addModeClass: true,
+					showHint: true,
+				}}
+				aria-label='code-editor'
+			/>
+			<p style={{ fontSize: '0.8rem' }} >
+				<strong>NOTE: </strong>Click on a Previously Saved Query to Begin.
+			</p>
+		</div>
+		<EditorPanel/>
+	</div>);
+	
+};
 
 export default QueryInput;
