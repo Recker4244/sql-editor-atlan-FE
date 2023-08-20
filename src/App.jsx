ac310809379849c6cfef2f9e6572571e87c81274
Recker4244 <ashutoshsenapati2311@gmail.com>
2023-08-20 21:01:29 +0530

    feat: code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React,{Suspense, lazy} from 'react';
 import './App.css';
 import { BrowserRouter, Route ,Routes} from 'react-router-dom';
-import Pages from './pages/index.js';
+
+const HomePage=lazy(() => import('./pages/HomePage/index.jsx'));
+const EditorPage=lazy(() => import('./pages/EditorPage/index.jsx'));
 
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path='*' element={<Pages.HomePage />} />
-					<Route path='/editor' element={<Pages.EditorPage />} />
-				</Routes>
-			</BrowserRouter>
+			<Suspense fallback={<div>Loading...</div>}>
+				<BrowserRouter>
+					<Routes>
+						<Route path='*' element={<HomePage/>} />
+						<Route path='/editor' element={<EditorPage />} />
+					</Routes>
+				</BrowserRouter>
+			</Suspense>
 		</div>
 	);
 }
