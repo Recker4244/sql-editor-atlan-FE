8dd4afe95448e8dbb5a1492759ede6bd0c92c5aa
Ashutosh <ashutoshsenapati2311@gmail.com>
2023-09-20 02:01:41 +0530

    feat: limiter for large data

--- src/components/Output/index.jsx
+++ src/components/Output/index.jsx
@@ -1,70 +1,88 @@
-import { useContext,React,useState} from 'react';
+import { useContext, React, useState } from 'react';
 import MainContext from '../../MainContext';
 import Table from '../Table/index';
 import { CSVLink } from 'react-csv';
 import './Output.css';
 import TableSchema from '../TableSchema';
 import TabBar from '../TabBar';
-import { FaDownload} from 'react-icons/fa';
+import { FaDownload } from 'react-icons/fa';
 import Visualisation from '../Visualisation';
+import { useEffect } from 'react';
 
 const Output = () => {
 	const { queryHistory } = useContext(MainContext);
-	const [tab,setTab]=useState(0);
+	const [tab, setTab] = useState(0);
+	const [isLimited,setIsLimited]= useState(false);
+	const [output,setOutput]=useState([]);
 	const exportData = () => {
 		console.log('Exporting Data');
 	};
-
-	const getTab=()=>{
+	useEffect(() => {
+		setOutput(queryHistory.outputData);
+	}, [queryHistory.outputData]);
+	const getTab = () => {
 		switch (tab) {
 		case 0:
-			return (<Table result={queryHistory.outputData}>
+			return (<Table result={output}>
 			</Table>);
 		case 1:
-			return (<TableSchema data={queryHistory.outputData}></TableSchema>);
+			return (<TableSchema data={output}></TableSchema>);
 
 		case 2:
-			return <Visualisation data={queryHistory.outputData}/>;
-		
+			return <Visualisation data={output} />;
+
 		default:
 			break;
 		}
+	};
+	const handleChange=(e)=>{
+		if(e.target.checked)
+			setOutput(queryHistory.outputData.slice(0, 100));
+		else
+			setOutput(queryHistory.outputData);
+		setIsLimited(e.target.checked);
 	};
 
 	return (
 		<div className='query-results'>
 			{queryHistory.outputData.length > 0 ? (
 				<>
-					<TabBar tab={tab} tabs={ ['Output','Table Structure','Visualisation']} onChangeFunc={setTab}></TabBar>
+					<TabBar tab={tab} tabs={['Output', 'Table Structure', 'Visualisation']} onChangeFunc={setTab}></TabBar>
 					<div className='query-details'>
-						
-						{tab==0||tab==1?<p>
 
-							<span>
-								{tab==0?queryHistory.outputData.length:Object.keys(queryHistory.outputData[0]).length}							
-							</span>{' '}
-            rows in Set
-							<span style={{ fontSize: '0.9rem' }} className='text-1'>
-								{' '}
-            (0.03sec)
-							</span>
-						</p>:null}
-						{tab==0||tab==1?
+						{tab == 0 || tab == 1 ? 
+							<div className='information'>
+								<p>
+									<span>
+										{tab == 0 ? output.length : Object.keys(queryHistory.outputData[0]).length}
+									</span>{' '}
+							rows in Set
+							
+								</p>
+								{queryHistory.outputData.length>=100?
+									<div className='limiter'>
+										<input checked={isLimited} type="checkbox" onChange={handleChange} />
+										<p>Limit to 100 rows</p>
+									</div>:null}
+								
+							</div>
+							: null}
+						{tab == 0 || tab == 1 ?
 							<div className='export-button-wrapper'>
 								<CSVLink className='export-button'
-									data={queryHistory.outputData}
+									data={output}
 									filename={'dataOutput.csv'}
 								>
 									<button onClick={exportData}>
-                  Export <FaDownload/>
+										Export <FaDownload />
 									</button>
 								</CSVLink>
-							</div>:null}
-						
+							</div> : null}
+
 					</div>
-					
+
 					{getTab()}
-					
+
 				</>
 			) : (
 				<div className='placeholder-text'>
